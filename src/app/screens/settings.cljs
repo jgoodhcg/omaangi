ddce3112a9c3dadd3bf74918f7e5342a6bd3f80f
justin <jgoodhcg@gmail.com>
2022-01-28 20:08:44 -0500

    Remove useless setting stuff

--- src/app/screens/settings.cljs
+++ src/app/screens/settings.cljs
@@ -26,48 +26,50 @@
 
           [:> paper/Surface {:style (merge  (tw "flex flex-1"))}
 
-           ;; switchties
            [:> rn/View {:style (tw "flex p-8 flex-col")}
-
-            [:> rn/View {:style (tw "flex flex-row justify-between mb-8")}
-             [:> paper/Text "Dark mode enabled"]
-             [:> paper/Switch {:value true :on-value-change #(tap> "switchity switch")}]]
-
-            [:> rn/View {:style (tw "flex flex-row justify-between mb-8")}
-             [:> paper/Text "Lock sessions by default"]
-             [:> paper/Switch {:value false :on-value-change #(tap> "switchity switch")}]]
-
-            [:> paper/Divider {:style (tw "mb-8")}]
-
-            ;; export
-            [:> rn/View {:style (tw "flex flex-col mb-8")}
-             [:> paper/Text {:style (tw "mb-4")} "Export"]
-             [:> rn/View {:style (tw "flex flex-row")}
-              [:> paper/Button {:style (tw "mr-4 w-24")
-                                :mode  "contained"
-                                :icon  "code-parentheses"} "edn"]
-              [:> paper/Button {:style (tw "mr-4 w-24")
-                                :mode  "contained"
-                                :icon  "code-braces"} "json"]
-              [:> paper/Button {:style (tw "mr-4 w-24")
-                                :mode  "contained"
-                                :icon  "code-brackets"} "csv"]]]
-
-            [:> paper/Divider {:style (tw "mb-8")}]
-
-            ;; airtable
-            [:> rn/View {:style (tw "flex flex-col mb-8")}
-             [:> paper/Text {:style (tw "mb-4")} "Airt Table Integration"]
-             [:> rn/View {:style (tw "flex flex-row")}
-              [:> paper/Button {:style    (tw "mr-4 w-32")
-                                :disabled true
-                                :mode     "outlined" ;; contained when key is added
-                                :icon     "key"} "add key"] ;; "remove key" when key is added
-              [:> paper/Button {:style    (tw "mr-4 w-32")
-                                :disabled true
-                                :mode     "outlined" ;; contained when key is added
-                                :icon     "refresh"} "sync"]]]
-
 
             [:> rn/View {:style (tw "flex flex-col mb-8")}
              [:> paper/Caption {} version]]]]]))]))
+
+(comment
+  ;; switchties
+  [:> rn/View {:style (tw "flex flex-row justify-between mb-8")}
+             [:> paper/Text "Dark mode enabled"]
+   [:> paper/Switch {:value true :on-value-change #(tap> "switchity switch")}]]
+
+  [:> rn/View {:style (tw "flex flex-row justify-between mb-8")}
+             [:> paper/Text "Lock sessions by default"]
+   [:> paper/Switch {:value false :on-value-change #(tap> "switchity switch")}]]
+
+  [:> paper/Divider {:style (tw "mb-8")}]
+
+            ;; export
+  [:> rn/View {:style (tw "flex flex-col mb-8")}
+   [:> paper/Text {:style (tw "mb-4")} "Export"]
+   [:> rn/View {:style (tw "flex flex-row")}
+    [:> paper/Button {:style (tw "mr-4 w-24")
+                      :mode  "contained"
+                      :icon  "code-parentheses"} "edn"]
+    [:> paper/Button {:style (tw "mr-4 w-24")
+                      :mode  "contained"
+                      :icon  "code-braces"} "json"]
+    [:> paper/Button {:style (tw "mr-4 w-24")
+                      :mode  "contained"
+                      :icon  "code-brackets"} "csv"]]]
+
+  [:> paper/Divider {:style (tw "mb-8")}]
+
+            ;; airtable
+  [:> rn/View {:style (tw "flex flex-col mb-8")}
+   [:> paper/Text {:style (tw "mb-4")} "Airt Table Integration"]
+   [:> rn/View {:style (tw "flex flex-row")}
+    [:> paper/Button {:style    (tw "mr-4 w-32")
+                      :disabled true
+                      :mode     "outlined" ;; contained when key is added
+                      :icon     "key"} "add key"] ;; "remove key" when key is added
+    [:> paper/Button {:style    (tw "mr-4 w-32")
+                      :disabled true
+                      :mode     "outlined" ;; contained when key is added
+                      :icon     "refresh"} "sync"]]]
+
+)
