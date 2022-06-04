5b73fd6c761375e9863a5d6f00cee869e3cc8f89
justin <jgoodhcg@gmail.com>
2022-06-04 10:20:48 -0400

    Add kit

---
+++ backend/src/clj/umeng/backend/core.clj
@@ -0,0 +1,46 @@
+(ns umeng.backend.core
+  (:require
+    [clojure.tools.logging :as log]
+    [integrant.core :as ig]
+    [umeng.backend.config :as config]
+    [umeng.backend.env :refer [defaults]]
+
+    ;; Edges
+    [xtdb.jdbc]
+    [kit.edge.db.xtdb]     
+    [kit.edge.server.undertow]
+    [umeng.backend.web.handler]
+
+    ;; Routes
+    [umeng.backend.web.routes.api]
+    )
+  (:gen-class))
+
+;; log uncaught exceptions in threads
+(Thread/setDefaultUncaughtExceptionHandler
+  (reify Thread$UncaughtExceptionHandler
+    (uncaughtException [_ thread ex]
+      (log/error {:what :uncaught-exception
+                  :exception ex
+                  :where (str "Uncaught exception on" (.getName thread))}))))
+
+(defonce system (atom nil))
+
+(defn stop-app []
+  ((or (:stop defaults) (fn [])))
+  (some-> (deref system) (ig/halt!))
+  (shutdown-agents))
+
+(defn start-app [& [params]]
+  ((or (:start params) (:start defaults) (fn [])))
+  (->> (config/system-config (or (:opts params) (:opts defaults) {}))
+       (ig/prep)
+       (ig/init)
+       (reset! system))
+  (.addShutdownHook (Runtime/getRuntime) (Thread. stop-app)))
+
+(defn -main [& _]
+  (start-app))
+
+(comment
+  ())
