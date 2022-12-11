90d926706a207120c1a72c4d3554ae85b9e427f4
justin <jgoodhcg@gmail.com>
2022-12-11 15:10:01 -0500

    Add example notebook using clay

---
+++ notebooks/src/umeng/notebooks/index.clj
@@ -0,0 +1,39 @@
+(ns umeng.notebooks.index
+  (:require [scicloj.clay.v2.api :as clay]
+            [scicloj.kindly.v3.api :as kindly]
+            [scicloj.kindly.v3.kind :as kind]
+            [scicloj.kindly-default.v1.api :as kindly-default]))
+
+(kindly-default/setup!)
+
+(clay/start!)
+
+(defn vega-point-plot [data]
+  (-> {:data {:values data},
+       :mark "point"
+       :encoding
+       {:size {:field "w" :type "quantitative"}
+        :x {:field "x", :type "quantitative"},
+        :y {:field "y", :type "quantitative"},
+        :fill {:field "z", :type "nominal"}}}
+      kind/vega))
+
+(defn random-data [n]
+  (->> (repeatedly n #(- (rand) 0.5))
+       (reductions +)
+       (map-indexed (fn [x y]
+                      {:w (rand-int 9)
+                       :z (rand-int 9)
+                       :x x
+                       :y y}))))
+
+(defn random-vega-plot [n]
+  (-> n
+      random-data
+      vega-point-plot))
+
+(random-vega-plot 2)
+
+(comment
+  (clay/show-doc! "src/umeng/notebooks/index.clj")
+  )
