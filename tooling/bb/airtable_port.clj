a72d8ea1ffb93326a653ea89b4c3654828e41136
justin <jgoodhcg@gmail.com>
2022-12-11 15:20:56 -0500

    Use airtable tooling to make some data files

--- tooling/bb/airtable_port.clj
+++ tooling/bb/airtable_port.clj
@@ -1,12 +1,13 @@
 (ns umeng.tooling.bb.airtable-port
   (:require [babashka.curl :as curl]
             [cheshire.core :as json]
-            [clojure.set :refer [rename-keys]]))
+            [clojure.set :refer [rename-keys]]
+            [clojure.pprint :refer [pprint]]))
 
 (def api-key (first *command-line-args*))
 
-(defn get-all-records [db-id table-id api-key]
-  (let [url      (str "https://api.airtable.com/v0/" db-id "/" table-id)]
+(defn get-all-records [base-id table-id api-key]
+  (let [url      (str "https://api.airtable.com/v0/" base-id "/" table-id)]
     (loop [response (-> (curl/get
                          url
                          {:headers {"Authorization" (str "Bearer " api-key)}})
@@ -24,13 +25,21 @@
                (json/parse-string))
            (concat records (-> response (get "records")))))))))
 
-(-> (get-all-records "appcu3LYk0kLQ2f6A" "tblXXCTTcI1jJjdIO" api-key)
+(def base-id "appcu3LYk0kLQ2f6A")
+
+(def table-ids {:exercise-log "tblXXCTTcI1jJjdIO"
+                :exercises    "tblDLGrkK6jfT2fPU"})
+
+(-> (get-all-records base-id (:exercise-log table-ids) api-key)
     #_(->> (map #(get % "fields")))
     #_(->> (apply merge))
-    first
+    #_first
     #_(rename-keys {})
     #_(spit "exercise-log.edn")
-    println
+    #_println
+    (#(with-out-str (pprint %)))
+    #_(->> (spit "2022_12_11__15_16_exercises.edn"))
+    (->> (spit "2022_12_11__15_17_exercise-log.edn"))
     )
 
 (comment
@@ -67,19 +76,4 @@
                   "notes"                  ""
                   "day"                    ""
                   "weight"                 ""}}
-
-  ;; shape of documents output
-  {:xt/id             #uuid "ec5b2c43-f7be-4603-b601-a9f6b64fd14b"
-   :type              :exercise-log
-   :exercise/id       #uuid "5a31abf1-5af9-4825-afc0-3206b80de0ed"
-   :timestamp         #inst "2022-10-24T09:20:27.966-00:00" ;; I'm tempted to use valid time instead
-   :duration          #time/duration "PT1M40S"
-   :notes             "Focused on keeping my quads engaged"
-   :relativety-score  :relativety-score/better
-   :exercise-log/data [{:sets 1 :reps 2 :weight 3 :weight-unit "lbs" :duration #time/duration "PT1M40S"}
-                           ;; could also include any of these keys
-                           ;; semantically only weight and weight-unit would be in either type
-                       {:distance 12 :distance-unit "miles" :elevation-gain "" :elevation-gain-unit "" :duration #time/duration "PT1M40S"}
-                           ;; this one is specific to inversion table but with a schemaless I can use whatever
-                       {:angle 60 :duration #time/duration "PT1M40S"}]}
 )
