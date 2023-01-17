3132e4a1ca50a2b332c889fdce4026023b75f544
justin <jgoodhcg@gmail.com>
2023-01-16 20:21:14 -0500

    Change argument parsing for airtable port

--- tooling/bb/airtable_port.clj
+++ tooling/bb/airtable_port.clj
@@ -2,11 +2,12 @@
   (:require [babashka.curl :as curl]
             [cheshire.core :as json]
             [clojure.set :refer [rename-keys]]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [babashka.cli :as cli]
+            [clojure.string :refer [replace]]
+            ))
 
-(def api-key (first *command-line-args*))
-
-(defn get-all-records [base-id table-id api-key]
+(defn get-all-records [{:keys [api-key base-id table-id]}]
   (let [url      (str "https://api.airtable.com/v0/" base-id "/" table-id)]
     (loop [response (-> (curl/get
                          url
@@ -25,55 +26,22 @@
                (json/parse-string))
            (concat records (-> response (get "records")))))))))
 
-(def base-id "appcu3LYk0kLQ2f6A")
+(defn timestamp []
+  (-> (java.time.LocalDateTime/now)
+      str
+      (replace "T" "__")
+      (replace ":" "_")
+      (replace "." "_")))
 
-(def table-ids {:exercise-log "tblXXCTTcI1jJjdIO"
-                :exercises    "tblDLGrkK6jfT2fPU"})
+(let [opts (cli/parse-opts
+            *command-line-args*
+            {:require [:api-key :base-id :table-id]
+             :alias   {:k :api-key
+                       :b :base-id
+                       :t :table-id}})]
 
-(-> (get-all-records base-id (:exercise-log table-ids) api-key)
-    #_(->> (map #(get % "fields")))
-    #_(->> (apply merge))
-    #_first
-    #_(rename-keys {})
-    #_(spit "exercise-log.edn")
-    #_println
+  (-> (get-all-records opts)
     (#(with-out-str (pprint %)))
-    #_(->> (spit "2022_12_11__15_16_exercises.edn"))
-    (->> (spit "2022_12_11__15_17_exercise-log.edn"))
-    )
-
-(comment
-  ;; all fields for exercise-log
-  '("total-weight"
-    "Angle"
-    "exercise"
-    "reps"
-    "timestamp"
-    "source (from exercise)"
-    "duration minutes"
-    "distance"
-    "better than normal set"
-    "duration"
-    "worse than normal set"
-    "notes"
-    "day"
-    "weight")
-
-  ;; shape of documents incoming
-  {"id"          "rec00CSCBqhgUJtxF"
-   "createdTime" "2022-02-28T20:18:52.000Z"
-   "fields"      {"total-weight"           ""
-                  "Angle"                  ""
-                  "exercise"               ""
-                  "reps"                   ""
-                  "timestamp"              ""
-                  "source (from exercise)" ""
-                  "duration minutes"       ""
-                  "distance"               ""
-                  "better than normal set" ""
-                  "duration"               ""
-                  "worse than normal set"  ""
-                  "notes"                  ""
-                  "day"                    ""
-                  "weight"                 ""}}
-)
+    (->> (spit (str (timestamp) "_"
+                    (-> opts :table-id (replace "-" "_"))
+                    ".edn")))))
